b03f1691c2c1bd7c321c157a6cbf774747986ce5
Dilli_Rao <dilliwebdev218@gmail.com>
2024-11-18 23:00:33 +0530

    Update factorial.js

--- 1-Mathematics/factorial.js
+++ 1-Mathematics/factorial.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* // Iterative Function
 function factorial(n) {
   if (n === 0 || n === 1) {
     return 1;
@@ -11,5 +12,36 @@ function factorial(n) {
   }
   return fact;
 }
-
 console.log(factorial(5));
+ */
+/* 
+// Recursive Function
+// Recusrsive function should always have a base case that will end the recursion
+
+function recursiveFactorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * recursiveFactorial(n - 1);
+}
+
+console.log(recursiveFactorial(3));
+ */
+
+// While loop function
+
+function whileFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  // Initialization of index and fact
+  let i = n,
+    fact = 1;
+  // Math.floor will give exact number and will terminate if equal to n = n
+  while (Math.floor(n / i) !== n) {
+    // fact = fact * i , i will decremetn each loop
+    fact *= i;
+    i--;
+  }
+  return fact;
+}
+
+console.log(whileFactorial(5));
